c43ae4ba584f4b77f67b4225e560e8ecb3d58e06
AmineZhioua <amine-zhioua@outlook.com>
2024-12-29 18:32:19 -0800

    Changes

--- technical-test/src/api.ts
+++ technical-test/src/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-
 // Fetch all courses
 export const getAllCourses = async () => {
   const response = await axios.get(`${API_URL}/courses/`);
